1cbf2f0016d124951a48f75383164dfbbce199d2
zeinrizky98 <100661845+zeinrizky98@users.noreply.github.com>
2022-06-24 13:48:43 +0700

    Update and rename youtube.R to tweet.R

---
+++ tweet.R
@@ -0,0 +1,44 @@
+library(mongolite)
+# This is the connection_string. You can get the exact url from your MongoDB cluster screen
+connection_string = Sys.getenv("MONGO_CONNECTION_STRING")
+
+twitter_collection <- mongo(collection = Sys.getenv("MONGO_COLLECTION_NAME"), # Creating collection
+                            db = Sys.getenv("MONGO_DB_NAME"), # Creating DataBase
+                            url = connection_string, 
+                            verbose = TRUE)
+
+a <- twitter_collection$find()
+a <- tail(a,3)
+
+# Hashtag
+hashtag <- c("Music", "Youtube", "Chart", "Hits", "Trend", "24Hours", "Videos",
+             "github","rvest","rtweet", "bot", "opensource", "ggplot2", "dplyr", "tidyr")
+
+samp_word <- sample(hashtag, 1)
+
+## Status Message
+
+status_details <- paste0(Sys.Date(),": ", "\n", "\n",
+                         "1. ",    a[1,3]," (", a[1,2], ") \n", 
+                         "2. ",    a[2,3]," (", a[2,2], ")\n",
+                         "3. ",    a[3,3]," (", a[3,2], ")\n", "\n",
+                         a$link[1], "\n",
+                         "#",samp_word, " #YoutubeChart", "\n")
+
+# Publish to Twitter
+library(rtweet)
+
+## Create Twitter token
+yutub_token <- rtweet::create_token(
+  app = "Muzik_Youtube",
+  consumer_key = Sys.getenv("TWITTER_API_KEY"), 
+  consumer_secret = Sys.getenv("API_KEY_SECRET"),
+  access_token = Sys.getenv("TWITTER_ACCESS_TOKEN"),
+  access_secret = Sys.getenv("TWITTER_ACCESS_TOKEN_SECRET")
+)
+
+
+## Post Twitter
+rtweet::post_tweet(
+  status = status_details,
+  token = yutub_token)
